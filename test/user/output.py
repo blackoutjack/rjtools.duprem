24b7b0cc83810db6716b96f4c630420216793091
Rich Joiner <rich@richjoiner.com>
2023-01-24 22:55:12 -0500

    Document and clean up tests

--- test/user/output.py
+++ test/user/output.py
@@ -1,6 +1,6 @@
-import os
+'''End-to-end user tests for duplicate detection in duprem'''
 
-from util import fs
+import os
 
 def prep(args):
     args.insert(0, "python3")
@@ -8,15 +8,20 @@ def prep(args):
     args.insert(2, "duprem")
     return args
 
-TEST_DIR = os.path.dirname(__file__)
-TEST_FILE_TREE = os.path.join(TEST_DIR, "filetree")
+TEST_DIR = os.path.join(os.path.dirname(__file__), "filetree")
 
 def get_filepath(filename):
-    return os.path.join(TEST_FILE_TREE, filename)
+    return os.path.join(TEST_DIR, filename)
 
+'''Basic test ensuring that empty files are detected as duplicates'''
 run_basic = prep([get_filepath("empty1.txt"), get_filepath("empty2.txt")])
 
 out_basic = '''Duplicate content:
-0: %TESTDIR%/filetree/empty1.txt
-1: %TESTDIR%/filetree/empty2.txt'''
+0: %TESTDIR%/empty1.txt
+1: %TESTDIR%/empty2.txt'''
+
+'''Negative test showing that differing files are not detected as duplicates'''
+run_no_dup = prep([get_filepath("empty1.txt"), get_filepath("basic.txt")])
+
+out_no_dup = 'INFO: No duplicate files found.'
 
